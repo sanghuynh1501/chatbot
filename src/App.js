93b0a26be7a7da86943fee4c537da5b71f511375
sang huynh <sanghuynhnt95@gmail.com>
2021-03-05 20:04:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Chat Bot
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="chat-box">
+          <div className="chat-content">
+
+          </div>
+          <div className="chat-input">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
